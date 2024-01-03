a3fdb8dfd3eee4cfb4269a36f845448d5b48f145
Jeepers-Gitters <platoff@gmail.com>
2024-01-03 16:36:26 +0300

    Minor changes

--- yaeatc-1.ps1
+++ yaeatc-1.ps1
@@ -10,7 +10,8 @@ $EACCFolder = "C:\Temp\EACC\"
 $LogFile = $EACCFolder + "log.txt"
 
 $TicketFields = @(4,5,30,30,20,10,16,5,20,30,2,1,17,5,10,10,5,5,5,1,16,7,1,2,10,5,40,40,10,10,10,10,1,2,2,2,30,5,10,1,17,30,5,5,5,5,5,6,6)
-$FieldsNames = @("TicketVersion", "CalledNumber", "ChargedNumber", "ChargedUserName", "ChargedCostCenter", "ChargedCompany", "ChargedPartyNode", "Subaddress", "CallingNumber", "CallType", "CostType", "EndDateTime", "ChargeUnits", "CostInfo", "Duration", "TrunkIdentity", "TrunkGroupIdentity", "TrunkNode", "PersonalOrBusiness", "AccessCode", "SpecificChargeInfo", "BearerCapability", "HighLevelComp", "DataVolume", "UserToUserVolume", "ExternalFacilities", "InternalFacilities", "CallReference", "SegmentsRate1", "SegmentsRate2", "SegmentsRate3", "ComType", "X25IncomingFlowRate", "X25OutgoingFlowRate", "Carrier", "InitialDialledNumber", "WaitingDuration", "EffectiveCallDuration", "RedirectedCallIndicator", "StartDateTime", "ActingExtensionNumber", "CalledNumberNode", "CallingNumberNode", "InitialDialledNumberNode", "ActingExtensionNumberNode", "TransitTrunkGroupIdentity", "NodeTimeOffset", "TimeDlt")
+$FieldsNames = @("TicketLabel", "TicketVersion", "CalledNumber", "ChargedNumber", "ChargedUserName", "ChargedCostCenter", "ChargedCompany", "ChargedPartyNode", "Subaddress", "CallingNumber", "CallType", "CostType", "EndDateTime", "ChargeUnits", "CostInfo", "Duration", "TrunkIdentity", "TrunkGroupIdentity", "TrunkNode", "PersonalOrBusiness", "AccessCode", "SpecificChargeInfo", "BearerCapability", "HighLevelComp", "DataVolume", "UserToUserVolume", "ExternalFacilities", "InternalFacilities", "CallReference", "SegmentsRate1", "SegmentsRate2", "SegmentsRate3", "ComType", "X25IncomingFlowRate", "X25OutgoingFlowRate", "Carrier", "InitialDialledNumber", "WaitingDuration", "EffectiveCallDuration", "RedirectedCallIndicator", "StartDateTime", "ActingExtensionNumber", "CalledNumberNode", "CallingNumberNode", "InitialDialledNumberNode", "ActingExtensionNumberNode", "TransitTrunkGroupIdentity", "NodeTimeOffset", "TimeDlt")
+$EmptyTicket = "01-00-01-00"
 $NormalTicket = "01-00-02-00"
 $TcktVersion = "ED5.2"
 $FieldsCounter = 1
@@ -160,8 +161,9 @@ switch ($data.Length) {
         $datastring = [System.Text.Encoding]::UTF8.GetString($data)
       }
     772 {
-        $datastring = [System.Text.Encoding]::ASCII.GetString($data)
-        $ProcessTicket = $datastring
+#        $datastring = [System.Text.Encoding]::ASCII.GetString($data)
+#        $ProcessTicket = $datastring
+        $ProcessTicket = [System.Text.Encoding]::ASCII.GetString($data)
         if ($TicketReady)
             {
             $TicketReady = $false
@@ -172,7 +174,7 @@ switch ($data.Length) {
             }
 #            $string
             )
-            Write-Host "Ticket Information:" $TicketForm.Length "fields processed"
+#            Write-Host "Ticket Information:" $TicketForm.Length "fields processed"
 
             $i = 0
     if ( $TicketForm[1] -ne $TcktVersion )
@@ -200,8 +202,9 @@ switch ($data.Length) {
     }
             }
         }
-        $datastring.Length
-
+    $TicketFlag = [System.BitConverter]::ToString($data[0..3])
+        Write-Host "Ticket Flag is " $TicketFlag
+        $datastring = $TicketFlag
     }
 
 
@@ -236,6 +239,14 @@ switch ($datastring)
             Write-Host "$MsgCounter. Reply with TEST_RSP"
             Write-Host -ForegroundColor Green "--- Running for" $TestKeepAlive.Elapsed.ToString('dd\.hh\:mm\:ss')
             }
+            }
+        $EmptyTicket
+            {
+              Write-Host "Ticket Information"
+            }
+        $NormalTicket
+            {
+              Write-Host "Ticket Information"
             }
         default {
         if ($datastring.Length -lt 772)
